372ed62b66f389e11ac54e23651e98dd934eaa56
Citrin <lima.stephanielemos@gmail.com>
2024-07-10 15:31:25 -0300

    initial commit

---
+++ screenmusic/src/main/java/br/com/citrinstar/screenmusic/ScreenmusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.citrinstar.screenmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmusicApplication.class, args);
+	}
+
+}
---
+++ screenmusic/src/test/java/br/com/citrinstar/screenmusic/ScreenmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.citrinstar.screenmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
